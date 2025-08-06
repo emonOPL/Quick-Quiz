fd5131d2b4bc5c6867bb31225d1c01c9aebc1281
Emon <emon.onnorokom@gmail.com>
2025-08-06 17:33:15 +0600

    Added Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { AiOutlineLogin, AiOutlineLogout } from "react-icons/ai";
+import { Link } from "react-router";
+
+export default function Navbar() {
+  return (
+    <div className="sticky top-0">
+      <div className="navbar bg-white shadow-sm text-black">
+        <div className="flex-1">
+          <Link to="/">
+            <div className="w-10 rounded-full">
+              <img alt="logo" src="./Logo.png" />
+            </div>
+          </Link>
+        </div>
+        <div className="flex gap-2 items-center">
+          <Link to="/" className="text-xl hover:underline">
+            User
+          </Link>
+          <Link to={"/auth/login"} className="text-2xl">
+            <span title="Login">
+              <AiOutlineLogin />
+            </span>
+          </Link>
+          <Link to={"/auth/logout"} className="text-2xl">
+            <span title="Logout">
+              <AiOutlineLogout />
+            </span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
